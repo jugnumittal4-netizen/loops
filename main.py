c72925849cfd6c15da14bddb8b63cd177b44b66c
jugnumittal4-netizen <jugnumittal4@gmail.com>
2026-07-19 14:09:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("-------------------Hi Welcome To Reverse order Program--------------------")
+n = int(input("enter a whole number which's reverse order you want to find: "))
+print("numbers from{0} to {1} are".format( n , 1 ))
+for i in range (n, 0, -1):
+    print(i)
